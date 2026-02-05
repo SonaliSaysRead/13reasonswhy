71a69ea50e81a6db29f922f1b464ec1d495ec058
SonaliSaysRead <sonali.sk97@gmail.com>
2026-02-05 20:57:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const reasons = [
-  "Your smile makes my whole day better 😍",
-  "You always know how to make me laugh 😂",
-  "I love how kind and thoughtful you are 💖",
-  "You make even boring days feel special ✨",
-  "Your hugs feel like home 🤗",
-  "I admire how passionate you are about the things you love 🌟",
-  "You support me in ways you don’t even realize 💕",
-  "I love how safe and happy I feel with you 🫶",
-  "You inspire me to be my best self 💪",
-  "Because life is just better with you in it ❤️"
+  "Your face. (Respectfully.) 😌",
+  "You make me laugh even when I’m trying to be mad 🙄😂",
+  "Your hugs have permanent-residence status 🤗",
+  "The way you say things like you invented language 🗣️✨",
+  "You’re hot. I’m brave for saying it. 🔥",
+  "You’re my favorite notification 📱💘",
+  "You’re weird in the exact compatible way 🤝😈",
+  "You make boring errands feel like a side quest 🧺🧙",
+  "You hype me up like I’m the main character 🎬💅",
+  "You’re kind, even when nobody’s watching 🫶",
+  "Your laugh is my favorite sound effect 😂🔊",
+  "You make me feel safe… AND feral. (Both.) 🐒💕",
+  "Because I love you. Unfortunately for you, this is lifelong. 😘"
 ];
 
 const modal = document.getElementById("modal");
